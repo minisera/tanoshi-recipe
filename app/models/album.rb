023e95a55b2af62a4ea058f007744bb4519dd181
minisera <koki.ohtaka0821@gmail.com>
2020-12-11 18:14:57 +0900

    トップページUI変更

--- app/models/album.rb
+++ app/models/album.rb
@@ -7,19 +7,19 @@ class Album < ApplicationRecord
   
   def self.search_type(type)
     data = {offset: 0,limit: total_count}
-    set_recipe_data(data)
+    result = set_recipe_data(data)
     return result[:cooking_records].select{|x| x[:recipe_type] == type}
   end
   
   def self.search_word(word)
     data = {offset: 0,limit: total_count}
-    set_recipe_data(data)
+    result = set_recipe_data(data)
     return result[:cooking_records].select{|x| x[:comment].include?(word)}
   end
   
   def self.pagenate(offset,limit)
     data = {offset: offset,limit: limit}
-    set_recipe_data(data)
+    result = set_recipe_data(data)
     return result[:cooking_records]
   end
 
@@ -27,10 +27,10 @@ class Album < ApplicationRecord
   
   # APIから、JSONを取得しハッシュに置き換える
   def self.set_recipe_data(data)
-    query.= data.to_query
+    query = data.to_query
     uri = URI.parse("https://cooking-records.herokuapp.com/cooking_records?"+query)
     json = Net::HTTP.get(uri)
-    result = JSON.parse(json,symbolize_names: true)
+    JSON.parse(json,symbolize_names: true)
   end
   
   # 現在のアルバムの合計を返す
