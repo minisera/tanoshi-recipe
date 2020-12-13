e2e9c3cddd478d11b76587506d953b3518ef1489
minisera <koki.ohtaka0821@gmail.com>
2020-12-14 07:05:30 +0900

    rubocop使用

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,22 +1,20 @@
 class AlbumsController < ApplicationController
-
   def index
     @albums = Album.index
   end
-  
+
   def search_type
     @albums = Album.search_type(params[:type])
   end
-  
+
   def search_word
     @albums = Album.search_word(params[:word])
   end
-  
+
   def pagenate
-    @albums = Album.pagenate(params[:offset],params[:limit])
+    @albums = Album.pagenate(params[:offset], params[:limit])
   end
 
   def show
   end
-
 end
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,43 +1,39 @@
 class Album < ApplicationRecord
-
   # トップページ
   def self.index
-    data = {offset: 0,limit: 50}
-    set_recipe_data(data)
-  end
-  
-  def self.search_type(type)
-    data = {offset: 0,limit: count_total}
-    set_recipe_data(data).select{|x| x[:recipe_type] == type}
-  end
-  
-  def self.search_word(word)
-    data = {offset: 0,limit: count_total}
-    set_recipe_data(data).select{|x| x[:comment].include?(word)}
-  end
-  
-  def self.pagenate(offset,limit)
-    data = {offset: offset,limit: limit}
+    data = { offset: 0, limit: 50 }
     set_recipe_data(data)
   end
 
-private
-  
+  def self.search_type(type)
+    data = { offset: 0, limit: count_total }
+    set_recipe_data(data).select { |x| x[:recipe_type] == type }
+  end
+
+  def self.search_word(word)
+    data = { offset: 0, limit: count_total }
+    set_recipe_data(data).select { |x| x[:comment].include?(word) }
+  end
+
+  def self.pagenate(offset, limit)
+    data = { offset: offset, limit: limit }
+    set_recipe_data(data)
+  end
+
   # 外部APIから、JSONを取得しハッシュを返す
   def self.set_recipe_data(data)
     query = data.to_query
-    uri = URI.parse("https://cooking-records.herokuapp.com/cooking_records?"+query)
+    uri = URI.parse('https://cooking-records.herokuapp.com/cooking_records?' + query)
     json = Net::HTTP.get(uri)
-    result = JSON.parse(json,symbolize_names: true)
+    result = JSON.parse(json, symbolize_names: true)
     result[:cooking_records]
   end
-  
+
   # 現在の記録数の合計を返す
   def self.count_total
-    uri = URI.parse("https://cooking-records.herokuapp.com/cooking_records")
+    uri = URI.parse('https://cooking-records.herokuapp.com/cooking_records')
     json = Net::HTTP.get(uri)
-    result = JSON.parse(json,symbolize_names: true)
+    result = JSON.parse(json, symbolize_names: true)
     result[:pagination][:total]
   end
-    
 end
